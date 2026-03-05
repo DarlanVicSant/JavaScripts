a82656c74c24839660ddb1526751482a06586a20
Sr.Linguiça <darlan.v.santos@ba.estudante.senai.br>
2026-03-05 16:47:31 -0300

    Add files via upload

---
+++ FUNÇAO.js
@@ -0,0 +1,12 @@
+// FUNÇAO COMUN NO JAVA SCRIPT
+
+function somar(a, b) {
+    return a + b
+}
+
+
+
+const soma = soma (2,3)
+
+console.log("soma: ${soma}")
+console.log
---
+++ switch-cases.js
@@ -0,0 +1,24 @@
+let opcao = 3
+
+switch (opcao) {
+    case 1:
+        console.log("Opção 1")
+        break;
+    case 2:
+        console.log("Opçao 2")
+        break;
+    case 3:
+        console.log("Opçao 3")
+        break;
+    case 4:
+        console.log("Opçao 4")
+        break;
+    case 5:
+        console.log("Opçao 5")
+        break;
+    
+    default:
+        console.log("Opçao invalida")
+        break
+
+}
